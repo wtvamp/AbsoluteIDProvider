ff05fbbc75b83fa0782c71411c386005b1c539e6
WTVamp <warrenjthompson@gmail.com>
2025-01-16 15:28:11 -0800

    Delete it all - fuck it we'll do it live.

--- BluetoothCredentialProvider/pch.cpp
+++
@@ -1,5 +0,0 @@
-// pch.cpp: source file corresponding to the pre-compiled header
-#pragma once
-#include "pch.h"
-
-// When you are using pre-compiled headers, this source file is necessary for compilation to succeed.
